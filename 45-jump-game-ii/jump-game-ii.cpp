d264dca9366d2ac9125f2a250abce7534296e58c
Tushar Sharma <109075669+TusharSharma811@users.noreply.github.com>
2025-08-06 21:46:43 +0530

    Time: 0 ms (100.00%) | Memory: 20.4 MB (62.55%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -1,39 +1,16 @@
 class Solution {
 public:
     int jump(vector<int>& nums) {
-        int n = nums.size() ;
-        if(n==1 )return 0 ;
-        
-         queue<int> q ; 
+        int n = nums.size();
+        int jumps = 0, current_end = 0, farthest = 0;
 
-         vector<int> vis(n , 0) ;
-
-         q.push(0) ;
-         vis[0]=1 ;
-         int count = 0 ; 
-
-         while(!q.empty()){
-            int sz = q.size() ;
-            for(int i=0 ; i<sz ; i++){
-                int l = q.front() ;
-                q.pop() ;
-                
-                for(int i=1 ; i<=nums[l] ; i++){
-                    int x = l+i ;
-                    if(x == n-1){
-                       return count+1 ; 
-                    }
-                    if(x<n && vis[x] == 0){
-                        q.push(x) ;
-                        vis[x] = 1;
-                    }
-                    else if(x == n-1){
-                        return count;
-                    }
-                }
+        for (int i = 0; i < n - 1; ++i) {
+            farthest = max(farthest, i + nums[i]);
+            if (i == current_end) {
+                ++jumps;
+                current_end = farthest;
             }
-            count++ ;
-         }
-         return -1 ;
+        }
+        return jumps;
     }
 };
