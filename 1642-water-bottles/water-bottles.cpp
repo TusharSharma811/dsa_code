ac1109078403f5e797fdacf0ebb87d367da73fdd
Tushar Sharma <109075669+TusharSharma811@users.noreply.github.com>
2025-10-01 13:26:44 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (11.04%) - LeetSync

---
+++ 1642-water-bottles/water-bottles.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int numWaterBottles(int numBottles, int numExchange) {
+        int ans = numBottles ;
+        int remain = 0 ;
+        while(numBottles >= numExchange){
+            remain = numBottles % numExchange ;
+            numBottles = numBottles/numExchange ;
+            ans += numBottles ;
+            numBottles += remain ;
+        }
+        return ans ;
+
+    }
+};
